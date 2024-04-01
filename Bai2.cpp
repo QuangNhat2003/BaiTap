5c788c2724636ee0dfba0f3f339d572c87be2712
Quang Nhất <151047596+QuangNhat2003@users.noreply.github.com>
2024-04-01 08:44:51 +0700

    Add files via upload

---
+++ Bai2.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+// Hàm kiểm tra xem một số có phải là số chính phương hay không
+int isPerfectSquare(int num) {
+    int squareRoot = sqrt(num);
+    return (squareRoot * squareRoot == num);
+}
+
+// Hàm đếm và in ra các số chính phương nhỏ hơn n
+void countAndPrintPerfectSquares(int n) {
+    printf("Cac so chinh phuong nho hon %d la:\n", n);
+    int count = 0;
+    for (int i = 1; i < n; i++) {
+        if (isPerfectSquare(i)) {
+            printf("%d ", i);
+            count++;
+        }
+    }
+    printf("\nTong so chinh phuong la: %d\n", count);
+}
+
+int main() {
+    int n;
+    printf("Nhap vao mot so nguyen duong: ");
+    scanf("%d", &n);
+    countAndPrintPerfectSquares(n);
+    return 0;
+}
