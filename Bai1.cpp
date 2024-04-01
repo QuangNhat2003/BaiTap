916f5e4ba6f17e62945d68f0240f0109829b1ff8
Quang Nhất <151047596+QuangNhat2003@users.noreply.github.com>
2024-04-01 08:37:15 +0700

    Add files via upload

---
+++ Bai1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main(void) {
+    printf("cac so nguyen co 2 chu so va la boi cua 7 la:\n");
+    for (int i = 10; i <= 99; i++) {
+        if (i % 7 == 0) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+    return 0;
+
+}
